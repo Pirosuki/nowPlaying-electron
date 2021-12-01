aec8cb48ff32516081c6ca796298948bd2f013a8
Pirosuki <pirosuki@outlook.com>
2021-12-01 11:17:11 +0100

    Added previously missing files

---
+++ popOutRenderer.js
@@ -0,0 +1,15 @@
+let songTitleSpan = document.getElementById("songTitle");
+let songArtistsSpan = document.getElementById("songArtists");
+let albumCoverElem = document.getElementById("albumCover");
+
+api.on('refreshSongInfo', function(songTitle, songArtists, albumCoverPath, event) {
+    // Refreshes album cover
+    albumCoverElem.src = albumCoverPath + "?" + Date.now();
+
+    // Waits for cover to load before changing text
+    albumCoverElem.addEventListener('load', function() {
+        // Refreshes title and artists
+        songTitleSpan.textContent = songTitle;
+        songArtistsSpan.textContent = songArtists;
+    })
+});
