ba1b944b7d894bd7d68114610ddf191fad9cdd9a
Pirosuki <pirosuki@outlook.com>
2021-12-08 14:10:30 +0100

    Progress with ui and text scroll

--- renderer.js
+++ renderer.js
@@ -1,10 +1,35 @@
+let homePage = document.getElementById('home');
+let optionsPage = document.getElementById('options');
+let aboutPage = document.getElementById('about');
+
+let homePageButton = document.getElementById('navButtonHome');
+homePageButton.onclick = function() {
+    optionsPage.style.display = 'none';
+    aboutPage.style.display = 'none';
+    homePage.style.display = 'block';
+}
+
+let optionsPageButton = document.getElementById('navButtonOptions');
+optionsPageButton.onclick = function() {
+    homePage.style.display = 'none';
+    aboutPage.style.display = 'none';
+    optionsPage.style.display = 'block';
+}
+
+let aboutPageButton = document.getElementById('navButtonAbout');
+aboutPageButton.onclick = function() {
+    homePage.style.display = 'none';
+    optionsPage.style.display = 'none';
+    aboutPage.style.display = 'block';
+}
+
 let refreshThemesButton = document.getElementById('refreshThemes');
 refreshThemesButton.onclick = function() {
-    api.send('triggerRefreshPopOutList');
+    //api.send('triggerRefreshPopOutList');
 }
 
 let popOutList = document.getElementById('popOutList');
-api.on('refreshPopOutList', function(list, event) {
+/*api.on('refreshPopOutList', function(list, event) {
     // Remove old values
     while (popOutList.hasChildNodes()) {
         popOutList.removeChild(popOutList.firstChild);
@@ -17,10 +42,36 @@ api.on('refreshPopOutList', function(list, event) {
         elem.textContent = theme;
         popOutList.appendChild(elem);
     }
-});
+});*/
 
 let popOutButton = document.getElementById('popOut');
 popOutButton.onclick = function() {
     let popOutTheme = popOutList.options[popOutList.selectedIndex].text;
-    api.send('popOut', popOutTheme);
-}
+    //api.send('popOut', popOutTheme);
+};
+
+let saveOptionsButton = document.getElementById('saveOptionsButton');
+saveOptionsButton.onclick = function() {
+    // save stuff
+};
+
+let optionArtistSeparatorPreview = document.getElementById('optionArtistSeparatorPreview');
+let optionArtistSeparatorInput = document.getElementById('optionArtistSeparatorInput');
+optionArtistSeparatorInput.onkeyup = function() {
+    let artistSeparator = optionArtistSeparatorInput.value;
+
+    let previewText = 'Artist1' + artistSeparator + 'Artist2'
+
+    optionArtistSeparatorPreview.innerText = previewText;
+};
+
+let optionCombinedFormattingPreview = document.getElementById('optionCombinedFormattingPreview');
+let optionCombinedFormattingInput = document.getElementById('optionCombinedFormattingInput');
+optionCombinedFormattingInput.onkeyup = function() {
+    let artistSeparator = optionArtistSeparatorInput.value;
+    let combinedFormatting = optionCombinedFormattingInput.value;
+
+    if (artistSeparator !== '' && combinedFormatting !== '' && combinedFormatting.includes('${artists}') && combinedFormatting.includes('${title}')) {
+        // send to main
+    }
+};
--- themes/popOut/darkSpotifyish/renderer.js
+++ themes/popOut/darkSpotifyish/renderer.js
@@ -1,12 +1,61 @@
+let root = document.documentElement;
+
 let songTitleSpan = document.getElementById("songTitle");
 let songArtistsSpan = document.getElementById("songArtists");
 let albumCoverElem = document.getElementById("albumCover");
 
-api.on('refreshPopOutSongInfo', function(isPlaying, songTitle, songArtists, albumCoverPath, event) {
+/*api.on('refreshPopOutSongInfo', function(isPlaying, songTitle, songArtists, albumCoverPath, event) {
     // Refreshes album cover
     albumCoverElem.src = albumCoverPath + '?' + Date.now();
 
     // Refreshes title and artists
     songTitleSpan.textContent = songTitle;
     songArtistsSpan.textContent = songArtists;
-});
+});*/
+
+let songTitle =  document.getElementById('songTitle');
+let songArtists =  document.getElementById('songArtists');
+let songTitleHidden =  document.getElementById('songTitleHidden');
+let songArtistsHidden =  document.getElementById('songArtistsHidden');
+
+
+let viewportWidth = document.documentElement.clientWidth;
+let titleWidth = songTitle.clientWidth;
+let artistsWidth = songArtists.clientWidth;
+
+console.log(viewportWidth + ' ' + titleWidth + ' ' + artistsWidth);
+
+if (titleWidth > viewportWidth * 0.8) {
+    let titleWidthConverted = Math.ceil(titleWidth / viewportWidth * 100);
+    let leftValue = 'calc(-' + titleWidthConverted + 'vw - 20vw)'
+    songTitleHidden.style.left = leftValue;
+
+    //root.style.setProperty('--titleWidth', leftValue);
+    songTitle.style.animation = 'periodicScroll 10s 3s linear infinite'
+    songTitleHidden.style.animation = 'periodicScroll 20s 5s linear infinite'
+    songTitleHidden.textContent = songTitle.textContent;
+    songTitleHidden.style.display = 'inline-flex';
+
+    console.log(songTitle.style.animation);
+}
+else {
+    songTitleHidden.style.display = 'none';
+    songTitleHidden.style.animation = 'none';
+    songTitle.style.animation = 'none';
+}
+
+if (artistsWidth > viewportWidth * 0.8) {
+    let artistsWidthConverted = Math.ceil(artistsWidth / viewportWidth * 100);
+    let leftValue = 'calc(-' + artistsWidthConverted + 'vw - 20vw)'
+    songArtistsHidden.style.left = leftValue;
+
+    songArtists.style.animation = 'periodicScroll 10s 3s linear infinite'
+    songArtistsHidden.style.animation = 'periodicScroll 20s 5s linear infinite'
+    songArtistsHidden.style.display = 'inline-flex';
+}
+else {
+    songArtistsHidden.style.display = 'none';
+    songArtistsHidden.style.animation = 'none'
+    songArtists.style.animation = 'none'
+
+}
